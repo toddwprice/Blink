dc0eb59582c24a4491dfedb872a36ca88ed68a66
Ian Pearson <ian@staywell>
2012-07-24 11:54:45 -0500

    First blink commit

---
+++ sketches/blink.ino
@@ -0,0 +1,99 @@
+/*
+  Web client
+ 
+ This sketch connects to a website (http://www.google.com)
+ using an Arduino Wiznet Ethernet shield. 
+ 
+ Circuit:
+ * Ethernet shield attached to pins 10, 11, 12, 13
+ 
+ created 18 Dec 2009
+ by David A. Mellis
+ 
+ */
+#define sclk 9
+#define mosi 8
+#define cs   7
+#define dc   6
+#define rst  5  // you can also connect this to the Arduino reset
+
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library
+#include <SPI.h>
+#include <Ethernet.h>
+
+Adafruit_ST7735 tft = Adafruit_ST7735(cs, dc, mosi, sclk, rst);
+
+// Enter a MAC address for your controller below.
+// Newer Ethernet shields have a MAC address printed on a sticker on the shield
+byte mac[] = {  0x90, 0xA2, 0xDA, 0x0D, 0x19, 0x58 };
+IPAddress server(10,249,201,20); // Google
+
+// Initialize the Ethernet client library
+// with the IP address and port of the server 
+// that you want to connect to (port 80 is default for HTTP):
+EthernetClient client;
+
+void setup() {
+    
+   tft.initR(INITR_REDTAB);
+   tft.fillScreen(ST7735_BLACK);
+  
+  // start the serial library:
+  Serial.begin(9600);
+  // start the Ethernet connection:
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    // no point in carrying on, so do nothing forevermore:
+    for(;;)
+      ;
+  }
+  // give the Ethernet shield a second to initialize:
+  delay(500);
+  Serial.println("connecting...");
+
+  // if you get a connection, report back via serial:
+  if (client.connect(server, 8181)) {
+    Serial.println("connected");
+    // Make a HTTP request:
+    client.println("GET /api/blink/2 HTTP/1.0");
+    client.println();
+  } 
+  else {
+    // kf you didn't get a connection to the server:
+    Serial.println("connection failed");
+  }
+   tft.setTextColor(ST7735_WHITE);
+   tft.setTextSize(2);
+   htft.setTextWrap(true);
+   tft.setCursor(1, -510);
+
+}
+
+void loop()
+{
+ 
+ 
+  // if there are incoming bytes available 
+  // from the server, read them and print them:
+  if (client.available()) {
+    char c = client.read();
+    Serial.print(c);
+    delay(3);
+    tft.print(c);
+ 
+  }
+
+
+  // if the server's disconnected, stop the client:
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
+
+    // do nothing forevermore:
+    for(;;)
+      ;
+  }
+}
+
