e13026b7d16a64f08654f1cd31c62d8e53408eb9
Todd Price <toddwprice@gmail.com>
2012-07-24 12:13:44 -0500

    fixed Serial.begin omission

--- sketches/aJson_test/aJson_test.ino
+++ sketches/aJson_test/aJson_test.ino
@@ -5,6 +5,8 @@ void setup()
   char* data = "{EditorialWorkload: 103, DesignWorkload: 105, AccountManagementWorkload: 95}";
   aJsonObject* jsonObject = aJson.parse(data);
   aJsonObject* ed = aJson.getObjectItem(jsonObject, "EditorialWorkload");
+  Serial.begin(9600);
+  Serial.println("Editorial:");
   Serial.println(ed->valueint);
 }
 
