40cac6eaad513ccc9047b6da4c96d5ab355a0133
Ian Pearson <ian@staywell>
2012-08-25 00:56:30 -0500

    final commit
    apparently I forgot to do this

--- sketches/blink/blink.ino
+++ sketches/blink/blink.ino
@@ -1,5 +1,7 @@
- /*
+/*
   Web client
+ 
+ this sketch requires an arduino mega, other boards don't have enough ram
  
  This sketch connects to the Staywell Customs internal site
  using an Arduino Wiznet Ethernet shield, gets data on workload 
@@ -23,12 +25,6 @@
 #define dc   6
 #define rst  5
 
-#define BUTTON_NONE 0
-#define BUTTON_DOWN 1
-#define BUTTON_RIGHT 2
-#define BUTTON_SELECT 3
-#define BUTTON_UP 4
-#define BUTTON_LEFT 5
 
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_ST7735.h> // Hardware-specific library
@@ -48,50 +44,33 @@ IPAddress server(10,249,201,20); //ev-web-01
 // with the IP address and port of the server 
 // that you want to connect to (port 80 is default for HTTP):
 EthernetClient client;
-String json = "";
-char ajson[600] = "";
+String json;
+char ajson[600];
 
 int print = 0; //variable to tell whether or not to store the web data
 
 int screen = 1;
 
-int month = 1;
-int year = 2012;
-int prevMonth = 1;
-int prevYear = 2012;
+int k;
 
-int loadPage = 0;
-
-int report = 0;
-
-
+char* months[] = {
+  "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul",
+  "Aug", "Sep", "Oct", "Nov", "Dec"};
 
 
 void setup() {
-  
+
   tft.initR(INITR_REDTAB);
   tft.fillScreen(ST7735_BLACK);
   tft.setTextColor(ST7735_WHITE); 
   tft.setTextSize(1);
-  tft.setCursor(0, 10);
-  
-  tft.println(" Up/Down selects");
-  tft.println(" the month");
-  tft.println(" Left/Right selects");
-  tft.println(" the year");
-  tft.println(" Current month ");
-  tft.print(" selection is ");
-  tft.println(month);
-  tft.println(" Current year");
-  tft.print(" selection is ");
-  tft.println(year);
+  tft.setCursor(0, 0);
+
+  attachInterrupt(0, buttonPress, CHANGE);
 
   Serial.begin(9600);
   Serial.println("Starting!");
-  
 
-  
-  
 
   // start the Ethernet connection:
   if (Ethernet.begin(mac) == 0) {
@@ -124,206 +103,236 @@ void setup() {
 
 void loop()
 {
-    //prepare the LCD to be printed to
 
-  
-  while( loadPage == 0 ) {
 
-    uint8_t b = readButton();
-    if (b == BUTTON_DOWN) {
-      month++;
-
+  //Serial.print("Got out of loop!");
+  // if there are incoming bytes available 
+  // from the server, read them and print them:
+  if (client.available()) {
+    char c = client.read();
+    //Serial.print(c);
+    if ( c == '{' ) { //wait until the json starts with a bracket
+      print = 1;
     }
-    if (b == BUTTON_LEFT) {
-      year--;
-
+    if (print == 1) { //save only the json
+      json += c;
     }
-    if (b == BUTTON_UP) {
-      month--;
 
-    }
-    if (b == BUTTON_RIGHT) {
-      year++;
+    //delay(3);
+    //tft.print(c);
 
+  }
+
+
+  // if the server's disconnected, stop the client:
+  if (!client.connected()) {
+    //Serial.println("Data loaded from webpage.");
+
+    //Serial.println(json);
+    //this will not appear on tft,
+    //it's only for debugging
+    //Serial.println();
+
+    //turn the string into a char array so it can be parsed
+    json.toCharArray(ajson, 500);
+
+    //find the data within the json, and create a variable with that data
+    aJsonObject* jsonObject = aJson.parse(ajson);
+
+    aJsonObject* month = aJson.getObjectItem(jsonObject, "CurrentMonth");
+    aJsonObject* ed1 = aJson.getObjectItem(jsonObject, "EdWorkload1");
+    aJsonObject* de1 = aJson.getObjectItem(jsonObject, "DeWorkload1");
+    aJsonObject* am1 = aJson.getObjectItem(jsonObject, "AMWorkload1"); 
+    aJsonObject* irfcount1 = aJson.getObjectItem(jsonObject, "IrfCount1");
+    aJsonObject* irfcost1 = aJson.getObjectItem(jsonObject, "IrfCost1");
+    aJsonObject* pages1 = aJson.getObjectItem(jsonObject, "Pages1");
+    aJsonObject* products1 = aJson.getObjectItem(jsonObject, "Products1");
+
+    aJsonObject* ed2 = aJson.getObjectItem(jsonObject, "EdWorkload2");
+    aJsonObject* de2 = aJson.getObjectItem(jsonObject, "DeWorkload2");
+    aJsonObject* am2 = aJson.getObjectItem(jsonObject, "AMWorkload2");
+    aJsonObject* irfcount2 = aJson.getObjectItem(jsonObject, "IrfCount2");
+    aJsonObject* irfcost2 = aJson.getObjectItem(jsonObject, "IrfCost2");
+    aJsonObject* pages2 = aJson.getObjectItem(jsonObject, "Pages2");
+    aJsonObject* products2 = aJson.getObjectItem(jsonObject, "Products2");
+
+    aJsonObject* ed3 = aJson.getObjectItem(jsonObject, "EdWorkload3");
+    aJsonObject* de3 = aJson.getObjectItem(jsonObject, "DeWorkload3");
+    aJsonObject* am3 = aJson.getObjectItem(jsonObject, "AMWorkload3");
+    aJsonObject* irfcount3 = aJson.getObjectItem(jsonObject, "IrfCount3");
+    aJsonObject* irfcost3 = aJson.getObjectItem(jsonObject, "IrfCost3");
+    aJsonObject* pages3 = aJson.getObjectItem(jsonObject, "Pages3");
+    aJsonObject* products3 = aJson.getObjectItem(jsonObject, "Products3");
+
+
+    int curMonth = month->valueint - 1;
+    Serial.println(curMonth);
+    int lastMonth = curMonth - 1;
+    int lst2Month = lastMonth - 1;
+
+    if(lastMonth < 0) {
+      lastMonth += 12;
     }
-    if (b == BUTTON_SELECT) {
-      loadPage = 1;
-      Serial.println("going to load webpage now...");
+    if(lst2Month < 0) {
+      lst2Month += 12;     
     }  
 
-    if (prevMonth != month || prevYear != year) {
-      tft.fillScreen(ST7735_BLACK);
-      tft.setTextSize(1);
-      tft.setCursor(0, 10);
-      tft.println(" Up/Down selects");
-      tft.println(" the month");
-      tft.println(" Left/Right selects");
-      tft.println(" the year");
-      tft.println(" Current month ");
-      tft.print(" selection is ");
-      tft.println(month);
-      tft.println(" Current year");
-      tft.print(" selection is ");
-      tft.println(year);
+    Serial.println(lastMonth);
+    Serial.println(lst2Month);
 
-    }
+    //again, debugging purposes
+    //Serial.println(ed1->valuefloat);
+    Serial.println("disconnecting.");  
 
-    prevMonth = month;
-    prevYear = year;
-    
-    if ( loadPage == 1 ) {
-      tft.print(" aaaa");
-    }
-  }
-   
+    client.stop();
 
-    //Serial.print("Got out of loop!");
-    // if there are incoming bytes available 
-    // from the server, read them and print them:
-    if (client.available()) {
-      char c = client.read();
-      //Serial.print(c);
-      if ( c == '{' ) { //wait until the json starts with a bracket
-        print = 1;
+
+    //loop through the screens forever
+
+    while(true) {
+
+      if(screen == 1) { 
+        
+        tft.setTextSize(1);
+        //print the data from the json
+        //currently only prints placeholder values 
+        tft.println();
+        tft.println(" Workload");
+        tft.println(" =================");
+
+        tft.print("     ");
+        tft.print(months[curMonth]);
+        tft.print("  ");
+        tft.print(months[lastMonth]);
+        tft.print("  ");
+        tft.println(months[lst2Month]);
+
+        tft.print(" ed: ");
+        tft.print(ed1->valuefloat, 0);
+        tft.print("% ");
+        tft.print(ed2->valuefloat, 0);
+        tft.print("% ");
+        tft.print(ed3->valuefloat, 0);
+        tft.println("%");
+
+        tft.print(" de: ");
+        tft.print(de1->valuefloat, 0);
+        tft.print("%  ");
+        tft.print(de2->valuefloat, 0);
+        tft.print("%  ");
+        tft.print(de3->valuefloat, 0);
+        tft.println("%");
+
+        tft.print(" am: ");
+        tft.print(am1->valuefloat, 0);
+        tft.print("%  ");
+        tft.print(am2->valuefloat, 0);
+        tft.print("%  ");
+        tft.print(am3->valuefloat, 0);
+        tft.println("%");
+
+        tft.println();
+        tft.println();
+        tft.println(" Pages");
+        tft.println(" =================");
+
+        tft.print(" ");
+        tft.print(months[curMonth]);
+        tft.print(": ");
+        tft.println(pages1->valueint);
+        tft.print(" ");
+        tft.print(months[lastMonth]);
+        tft.print(": ");
+        tft.println(pages2->valueint);
+        tft.print(" ");
+        tft.print(months[lst2Month]);
+        tft.print(": ");
+        tft.println(pages3->valueint);
+        
+        screen = 3;
+
       }
-      if (print == 1) { //save only the json
-        json += c;
-      }
+      if(screen == 2) {
+
+        tft.println();
+        tft.println(" IRF Count");
+        tft.println(" =================");
+        tft.print(" ");
+        tft.print(months[curMonth]);
+        tft.print("  ");
+        tft.print(months[lastMonth]);
+        tft.print("  ");
+        tft.println(months[lst2Month]);
+
+        tft.print(" ");
+        tft.print(irfcount1->valueint);
+        tft.print("    ");
+        tft.print(irfcount2->valueint);
+        tft.print("     ");
+        tft.println(irfcount3->valueint);
+
+        tft.println();
+        tft.println();
+        tft.println(" IRF Cost");
+        tft.println(" =================");
+
+        tft.print(" ");
+        tft.print(months[curMonth]);
+        tft.print(": ");
+        tft.println(irfcost1->valuefloat);
+
+        tft.print(" ");
+        tft.print(months[lastMonth]);
+        tft.print(": ");
+        tft.println(irfcost2->valuefloat);
+
+        tft.print(" ");
+        tft.print(months[lst2Month]);
+        tft.print(": ");
+        tft.println(irfcost3->valuefloat);
+
+        tft.println();
+        tft.println();
+        tft.println(" Products");
+        tft.println(" =================");
+
+        tft.print(" ");
+        tft.print(months[curMonth]);
+        tft.print(": ");
+        tft.println(products1->valueint);
+        tft.print(" ");
+        tft.print(months[lastMonth]);
+        tft.print(": ");
+        tft.println(products2->valueint);
+        tft.print(" ");
+        tft.print(months[lst2Month]);
+        tft.print(": ");
+        tft.println(products3->valueint);
       
-      //delay(3);
-      //tft.print(c);
-
-    }
-
-
-    // if the server's disconnected, stop the client:
-    if (!client.connected()) {
-      //Serial.println("Data loaded from webpage.");
-      
-      Serial.println(json); //this will not appear on tft, it's only for debugging
-      Serial.println();
-
-      //turn the string into a char array so it can be parsed
-      json.toCharArray(ajson, 700);
-
-      //find the data within the json, and create a variable with that data
-      aJsonObject* jsonObject = aJson.parse(ajson);
-      aJsonObject* ed = aJson.getObjectItem(jsonObject, "AmountApproved");
-
-      //again, debugging purposes
-      Serial.println("Amount Approved:");
-      Serial.println(ed->valuefloat);
-      Serial.println("disconnecting.");  
-
-      client.stop();
-
-
-      //loop through the screens forever
-
-      while(true) {
-
-        if(screen == 1) { 
-          tft.fillScreen(ST7735_BLACK);
-          tft.setCursor(0, 0);
-          tft.setTextSize(2);
-          //print the data from the json
-          //currently only prints placeholder values 
-          tft.println();
-          tft.println(" Workload");
-          tft.println(" ========");
-
-          tft.print(" ed: 100%");
-          tft.setTextSize(1);
-          tft.println();
-          tft.println();
-
-          tft.setTextSize(2);
-          tft.print(" de: 100%");
-          tft.setTextSize(1);
-          tft.println();
-          tft.println();
-
-          tft.setTextSize(2);
-          tft.println(" am: 100%");
-          tft.print(" ");
-          tft.println(ed->valuefloat);
-         /* tft.setTextSize(1);
-          tft.println();
-          tft.println();
-
-          tft.println(" Current month ");
-          tft.print(" selection is ");
-          tft.println(month);
-          tft.println(" Current year");
-          tft.print(" selection is ");
-          tft.println(year);
-        */
-          screen = 2;
-          delay(3000);
-        }
-        if(screen == 2) {
-          tft.fillScreen(ST7735_BLACK);
-          tft.setTextSize(2);
-          tft.setCursor(0, 0);
-
-          tft.println();
-          tft.println(" Placeholder Page 2");
-         /* tft.setTextSize(1);
-          tft.println();
-          tft.println();
-
-          tft.println(" Current month ");
-          tft.print(" selection is ");
-          tft.println(month);
-          tft.println(" Current year");
-          tft.print(" selection is ");
-          tft.println(year);
-        */
-          screen = 3;
-          delay(3000);
-        }  
-        if(screen == 3) {
-          tft.fillScreen(ST7735_BLACK);
-          tft.setTextSize(2);
-          tft.setCursor(0, 0);
-          tft.println();
-          tft.println(" Placeholder Page 3");
-          tft.println(" This is cooler than Placeholder Page 2");
-          tft.setTextSize(1);
-         /* tft.setTextSize(1);
-          tft.println();
-          tft.println();
-
-          tft.println(" Current month ");
-          tft.print(" selection is ");
-          tft.println(month);
-          tft.println(" Current year");
-          tft.print(" selection is ");
-          tft.println(year);
-        */
-          screen = 1;
-          delay(3000);
-        }
+        screen = 4;
+        
       }
-      for(;;)
+    }
+    for(;;)
       ;
 
-    }
   }
-  
-  uint8_t readButton(void) {
-  float a = analogRead(3);
-
-  a *= 5.0;
-  a /= 1024.0;
-
-  //Serial.print("Button read analog = ");
-  //Serial.println(a);
-  if (a < 0.2) return BUTTON_DOWN;
-  if (a < 1.0) return BUTTON_RIGHT;
-  if (a < 1.5) return BUTTON_SELECT;
-  if (a < 2.0) return BUTTON_UP;
-  if (a < 3.2) return BUTTON_LEFT;
-  else return BUTTON_NONE;
 }
+
+void buttonPress() {
+  if(screen == 3) {
+    tft.fillScreen(ST7735_BLACK);
+    tft.setCursor(0, 0);
+    screen = 2;
+  }
+  if(screen == 4) {
+    tft.fillScreen(ST7735_BLACK);
+    tft.setCursor(0, 0);
+    screen = 1;
+  }
+}  
+
+
+
+
 
